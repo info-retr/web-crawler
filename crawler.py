293bf32d6d992dbe5a55e3f1035663eb88bc0400
Julian Puffer <pufferj@uci.edu>
2023-02-03 17:19:51 -0800

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -12,7 +12,6 @@ from PartA import tokenize, compute_word_frequencies, tokenize_file
 
 logger = logging.getLogger(__name__)
 
-# RABBIT_HOLE_LIMIT = 200
 URL_LEN_LIMIT = 200
 
 class Crawler:
@@ -53,13 +52,8 @@ class Crawler:
 
             for next_link in self.extract_next_links(url_data):
                 if self.is_valid(next_link):
-                    print(url)
                     if self.corpus.get_file_name(next_link) is not None:
                         self.frontier.add_url(next_link)
-                        rabbit_hole_count += 1
-                        # if rabbit_hole_count > RABBIT_HOLE_LIMIT:
-                        #     del frontier[-RABBIT_HOLE_LIMIT+1]
-                        #     rabbit_hole_count = 0
             
         self.write_analytics()
 
@@ -92,10 +86,11 @@ class Crawler:
         if parsed.scheme not in set(["http", "https"]):
             return False
         try:
+            # ============start trap detection===========
+            if len(url) > URL_LEN_LIMIT or '#' in url: 
+                return False
 
-            # tokenize url to get subdomain, domain, query, etfc
-
-
+            # ============end trap detection=============
             return ".ics.uci.edu" in parsed.hostname \
                    and not re.match(".*\.(css|js|bmp|gif|jpe?g|ico" + "|png|tiff?|mid|mp2|mp3|mp4" \
                                     + "|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf" \
